8101b24a18547d2aba57356fe53e93c7a639179b
m4rk1sov <134860120+m4rk1sov@users.noreply.github.com>
2024-07-07 13:05:05 +0500

    Added templates

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,1 +1,17 @@
-package web
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	log.Println("Starting the server on :4000")
+	err := http.ListenAndServe("localhost:4000", mux)
+	log.Fatal(err)
+}
