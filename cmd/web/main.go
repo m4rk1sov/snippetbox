35b6d6dea6949cb85837ddda30726165b6366185
m4rk1sov <134860120+m4rk1sov@users.noreply.github.com>
2024-07-19 18:28:03 +0500

    Added configuration env

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,45 @@
 package main
 
 import (
+	"flag"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	// .env variables initialization
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
+	netAddress := os.Getenv("NET_ADDR")
+	staticDir := os.Getenv("STATIC_DIR")
+
+	// use .env variables | .String to convert user input of flag
+	addr := flag.String("addr", netAddress, "http network address")
+	if addr == nil {
+		log.Fatalf("missing env var: %s", netAddress)
+	}
+
+	// This command is responsible for taking command line value and assigning it
+	// to a values for flag, call before usage of variables of flag.
+	flag.Parse()
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	// path to static assets directory in .env file
+	fileServer := http.FileServer(http.Dir(staticDir))
+	if fileServer == nil {
+		log.Fatal("Error creating file server")
+	}
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting the server on :4000")
-	err := http.ListenAndServe("localhost:4000", mux)
+	// Returning the address of addr variable
+	log.Printf("Starting the server on %s", *addr)
+	err = http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module m4rk1sov/snippetbox
 
 go 1.22
+
+require github.com/joho/godotenv v1.5.1 // indirect
