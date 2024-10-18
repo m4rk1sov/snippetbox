3b54b75a32fc6fc6727d72ad4f555631d2b85cad
m4rk1sov <134860120+m4rk1sov@users.noreply.github.com>
2024-10-18 22:01:00 +0500

    restful routing

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,10 +9,12 @@ import (
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
+
+	// Became obsolete from new router pat
+	//if r.URL.Path != "/" {
+	//	app.notFound(w)
+	//	return
+	//}
 
 	s, err := app.snippets.Latest()
 	if err != nil {
@@ -27,7 +29,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -49,12 +51,18 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(""))
+}
+
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
+
+	// Is now obsolete because of new router
+	//if r.Method != http.MethodPost {
+	//	w.Header().Set("Allow", http.MethodPost)
+	//	app.clientError(w, http.StatusMethodNotAllowed)
+	//	return
+	//}
 
 	title := "0 snail"
 	content := "0 snail\nClimb Mount Fuji\nBut slowly, slowly!\n\n- Kobayashi Issa"
@@ -66,5 +74,5 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
 }
